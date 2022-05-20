5df8341891cdb569e63a6dfcc8c9286e47218a85
asia3211 <joanna.blaszka@o2.pl>
2022-05-20 15:24:16 +0200

    Add project files.

---
+++ GUI.cpp
@@ -0,0 +1,68 @@
+///////////////////////////////////////////////////////////////////////////
+// C++ code generated with wxFormBuilder (version 3.10.1-0-g8feb16b3)
+// http://www.wxformbuilder.org/
+//
+// PLEASE DO *NOT* EDIT THIS FILE!
+///////////////////////////////////////////////////////////////////////////
+
+#include "GUI.h"
+
+///////////////////////////////////////////////////////////////////////////
+
+MyFrame1::MyFrame1(wxWindow* parent, wxWindowID id, const wxString& title, const wxPoint& pos, const wxSize& size, long style) : wxFrame(parent, id, title, pos, size, style)
+{
+	this->SetSizeHints(wxDefaultSize, wxDefaultSize);
+
+	wxBoxSizer* bSizer1;
+	bSizer1 = new wxBoxSizer(wxHORIZONTAL);
+
+	wxBoxSizer* bSizer2;
+	bSizer2 = new wxBoxSizer(wxVERTICAL);
+
+	bSizer2->SetMinSize(wxSize(200, 533));
+	m_button_load = new wxButton(this, wxID_ANY, wxT("Load image!"), wxDefaultPosition, wxDefaultSize, 0);
+	bSizer2->Add(m_button_load, 0, wxEXPAND | wxTOP | wxLEFT, 5);
+
+	m_button_method_A = new wxButton(this, wxID_ANY, wxT("Method A"), wxDefaultPosition, wxDefaultSize, 0);
+	bSizer2->Add(m_button_method_A, 0, wxEXPAND | wxTOP | wxLEFT, 5);
+
+	m_button_methond_B = new wxButton(this, wxID_ANY, wxT("Method B"), wxDefaultPosition, wxDefaultSize, 0);
+	bSizer2->Add(m_button_methond_B, 0, wxEXPAND | wxTOP | wxLEFT, 5);
+
+
+	bSizer1->Add(bSizer2, 0, wxEXPAND | wxRIGHT, 5);
+
+	wxBoxSizer* bSizer3;
+	bSizer3 = new wxBoxSizer(wxVERTICAL);
+
+	bSizer3->SetMinSize(wxSize(800, 533));
+	m_scrolledWindow = new wxScrolledWindow(this, wxID_ANY, wxDefaultPosition, wxSize(800, 533), wxHSCROLL | wxVSCROLL);
+	m_scrolledWindow->SetScrollRate(5, 5);
+	m_scrolledWindow->SetForegroundColour(wxSystemSettings::GetColour(wxSYS_COLOUR_BTNTEXT));
+	m_scrolledWindow->SetBackgroundColour(wxSystemSettings::GetColour(wxSYS_COLOUR_BTNTEXT));
+
+	bSizer3->Add(m_scrolledWindow, 1, wxEXPAND | wxALL, 5);
+
+
+	bSizer1->Add(bSizer3, 1, wxEXPAND, 5);
+
+
+	this->SetSizer(bSizer1);
+	this->Layout();
+
+	this->Centre(wxBOTH);
+
+	// Connect Events
+	m_button_load->Connect(wxEVT_COMMAND_BUTTON_CLICKED, wxCommandEventHandler(MyFrame1::m_button_save_click), NULL, this);
+	m_button_method_A->Connect(wxEVT_COMMAND_BUTTON_CLICKED, wxCommandEventHandler(MyFrame1::m_button_method_A_click), NULL, this);
+	m_button_methond_B->Connect(wxEVT_COMMAND_BUTTON_CLICKED, wxCommandEventHandler(MyFrame1::m_button_method_A_click), NULL, this);
+}
+
+MyFrame1::~MyFrame1()
+{
+	// Disconnect Events
+	m_button_load->Disconnect(wxEVT_COMMAND_BUTTON_CLICKED, wxCommandEventHandler(MyFrame1::m_button_save_click), NULL, this);
+	m_button_method_A->Disconnect(wxEVT_COMMAND_BUTTON_CLICKED, wxCommandEventHandler(MyFrame1::m_button_method_A_click), NULL, this);
+	m_button_methond_B->Disconnect(wxEVT_COMMAND_BUTTON_CLICKED, wxCommandEventHandler(MyFrame1::m_button_method_A_click), NULL, this);
+
+}
---
+++ GUIMyFrame1.cpp
@@ -0,0 +1,18 @@
+#include "GUIMyFrame1.h"
+
+GUIMyFrame1::GUIMyFrame1( wxWindow* parent )
+:
+MyFrame1( parent )
+{
+
+}
+
+void GUIMyFrame1::m_button_save_click( wxCommandEvent& event )
+{
+// TODO: Implement m_button_save_click
+}
+
+void GUIMyFrame1::m_button_method_A_click( wxCommandEvent& event )
+{
+// TODO: Implement m_button_method_A_click
+}
